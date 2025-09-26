2344127bfcd0368e63b2ddd6c386402a07a63130
emybatista21 <emybatista159@gmail.com>
2025-09-26 00:20:58 -0300

    inicializando o projeto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,42 @@
+import { Routes } from "@angular/router";
+import { CadastroAlertas } from "./pages/cadastro-alertas/cadastro-alertas";
+import { Home } from "./pages/home/home";
+import { ListarAlertas } from "./pages/listar-alertas/listar-alertas";
+
+export const routes: Routes = [
+  // Rota para a Página Inicial
+  {
+    path: 'home',
+    // 2. Uso da propriedade 'component'
+    component: Home 
+  },
+  
+  // Rota para a Lista de Denúncias
+  {
+    path: 'denuncias',
+    component: ListarAlertas
+  },
+  
+  // Rota para o Formulário de Cadastro
+  {
+    path: 'cadastro',
+    component: CadastroAlertas
+  },
+
+  // ---------------------------------------------
+  // ROTAS AUXILIARES
+  // ---------------------------------------------
+
+  // Rota Padrão: Redireciona a URL base (/) para /home
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  
+  // Rota Curinga: Redireciona para /home
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
