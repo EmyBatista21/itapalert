2f9b0554adda4bf293e13eb34174e825c39c8d07
emybatista21 <emybatista159@gmail.com>
2025-09-26 00:21:23 -0300

    Criando componentes e definincdo rotas

---
+++ src/app/pages/cadastro-alertas/cadastro-alertas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-alertas',
+  imports: [],
+  templateUrl: './cadastro-alertas.html',
+  styleUrl: './cadastro-alertas.css'
+})
+export class CadastroAlertas {
+
+}
---
+++ src/app/pages/listar-alertas/listar-alertas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-alertas',
+  imports: [],
+  templateUrl: './listar-alertas.html',
+  styleUrl: './listar-alertas.css'
+})
+export class ListarAlertas {
+
+}
